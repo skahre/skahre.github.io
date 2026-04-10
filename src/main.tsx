528e9f5a27fd49981f8f1419efc10f1df05112ed
Sandra Kåhre <sandra.kahre@gmail.com>
2026-04-10 14:33:35 +0200

    Add bypass gh-pages routing

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+// Handle GitHub Pages 404 redirect
+(function () {
+  var redirect = sessionStorage.redirect;
+  delete sessionStorage.redirect;
+  if (redirect && redirect !== window.location.pathname) {
+    history.replaceState(null, "", redirect);
+  }
+})();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
