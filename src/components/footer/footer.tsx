3bf09bdb818c269e03e04f3bdfb2241c0b7a7478
Sandra Kåhre <sandra.kahre@gmail.com>
2026-04-20 07:34:16 +0200

    Fix footer basic styling

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,21 +2,21 @@ import LinkedInIcon from "../../assets/linkedin.tsx";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col nav:flex-row items-center justify-between bg-espresso text-white p-[36px] nav:px-[96px] nav:py-[36px] 3xl:px-[176px] 3xl:py-[48px] max-nav:gap-[24px]">
-      <h3>I design. I build.</h3>
-      <div className="flex flex-col items-center gap-[8px]">
+    <footer className="flex flex-col nav:flex-row items-center justify-between bg-sage-600 text-white p-[36px] nav:px-[96px] nav:py-[36px] 3xl:px-[176px] 3xl:py-[48px] max-nav:gap-[24px]">
+      <h2 className="font-body font-bold">I design. I build.</h2>
+      <div className="flex flex-col items-center gap-[8px] text-white/70">
         <p>last updated {__BUILD_DATE__}</p>
         <p className="text-center text-[10px] md:text-[12px] 3xl:text-[24px]">
-          © 2026 Sandra Kåhre — Designed & coded from scratch
+          © 2026 Sandra Kåhre • Designed & coded from scratch
         </p>
       </div>
-      <span className="flex flex-row items-center gap-[16px]">
+      <span className="flex flex-row items-center text-peach gap-[16px]">
         <LinkedInIcon
-          fill="var(--color-white)"
+          fill="currentColor"
           size="24px"
           aria-label="LinkedIn Logo"
         />
-        <h4>/sandra-kahre</h4>
+        <h3 className="font-body font-semibold">/sandra-kahre</h3>
       </span>
     </footer>
   );
